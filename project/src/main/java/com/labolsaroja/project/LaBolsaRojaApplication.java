6fe9b71ef8603bdc49934a03b84a645bea7e55f8
Fer Gomez <jorgegomezrios@gmail.com>
2023-05-17 20:08:19 -0600

    Se agregaron todos los arhivos del front y del back end

---
+++ project/src/main/java/com/labolsaroja/project/LaBolsaRojaApplication.java
@@ -0,0 +1,13 @@
+package com.labolsaroja.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBolsaRojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBolsaRojaApplication.class, args);
+	}
+
+}
