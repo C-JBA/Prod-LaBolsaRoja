48db90366bcfb102a70db202d55ef83b69e7f391
CJBA <barajasavilacristobaljair@gmail.com>
2023-05-18 19:17:20 -0600

    Se cargan imágenes por url

--- bin/main/static/js/productos.js
+++ bin/main/static/js/productos.js
@@ -25,16 +25,15 @@ localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
 let im;
-if (localStorage.getItem("productos") == null && cardGroup[0].childElementCount === 0) {
+localStorage.removeItem("productos");
 fetch('/api/producto/',{method:'GET'})
     .then(response => response.json())
     .then(data => {
 		
-		console.log(data);
      localStorage.setItem("productos", JSON.stringify(data));
       productos = data;
       productos.forEach((element,index) => {
-		im=src+element.img;
+		im=element.img;
         let html =
           `<div class="col ">
                     <div class="card h-350 ">
@@ -82,57 +81,5 @@ fetch('/api/producto/',{method:'GET'})
       });
     })
     .catch(error => {
-      console.error('Error al leer el archivo JSON:', error);
+      console.error('Error al leer la base de datos:', error);
     });
-} else {
-
-  productos = JSON.parse(localStorage.getItem("productos"));
-  productos.forEach((element,index) => {
-	  im=src+element.img;
-    let html =
-      `<div class="col ">
-                    <div class="card h-350 ">
-                        <img  src=${im} class="card-img-top card-image">
-                            <div class="card-body">
-                                <h5 class="card-title">${element.nombre}</h5>
-                                <p class ="card-text">${element.descripcion}</p>
-                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#openModal${index}">
-                                Ver ítem</button>
-                </div>
-        </div> </div>
-
-
-        <div class="modal" tabindex="-1" id="openModal${index}">
-        <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">${element.nombre}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          <br><p>
-            <img  src=${im} class="card-img-top card-image">
-            </p>
-            
-<div class="row g-2">
-  <div class="col-sm-6">
-    <a>Precio: ${element.precio}</a>
-  </div>
-  <div class="col-sm">
-  <a>Cantidad: <input type="number" id="cantidad${index}" class="form-control" placeholder="" aria-label="State" value="1" max="5" min ="1"></a>
-    
-  </div>
-</div>
-
-          </div>
-          <div class="modal-footer">
-            <button type="button" id="button3" class="btn btn-danger" data-bs-dismiss="modal" onclick="agregarAlCarrito(${index})" >Añadir al Carrito</button>
-          
-          </div>
-        </div>
-      </div>
-    </div>
-                    `;
-    cardGroup[0].insertAdjacentHTML("beforeend", html);
-  });
-}
