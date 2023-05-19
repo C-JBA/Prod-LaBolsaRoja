172e842a0ac28d521a092d5dc8144ca074d9c78d
Cristobal B <55607636+C-JBA@users.noreply.github.com>
2023-05-18 21:30:58 -0600

    Update loginController.java

--- src/main/java/com/labolsaroja/project/controller/loginController.java
+++ src/main/java/com/labolsaroja/project/controller/loginController.java
@@ -47,21 +47,15 @@ public Usuario loginUsuario(@RequestParam String email ,@RequestParam String con
 }//loginUsuario
 
 
-//@PostMapping
-//public Usuario passwordReset(@RequestParam String email) {
-//	
-//	Optional<Usuario> userByEmail=
-//			usuarioRepository.findByEmail(email);
-//	if (userByEmail.isPresent()) {
-//		Usuario user=userByEmail.get();
-//		
-//			if(user.getEmail().equals(email)){ 
-//			return user;
-//			}
-//	}//if isPresent
-//	return null;
-//	
-//}//passwordReset
+@PostMapping
+public Usuario passwordReset(@RequestParam String email) {
+	
+	return usuarioService.passwordReset(email);
+	
+}//passwordReset
+
+
+
 
 
 
